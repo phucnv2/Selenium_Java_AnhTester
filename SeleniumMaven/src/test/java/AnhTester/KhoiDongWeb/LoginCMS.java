5382737665170009620e4445ffd110dba062c943
phucnv2 <nguyenvanphuc20031996@gmail.com>
2023-05-09 00:04:55 +0700

    update

---
+++ SeleniumMaven/src/test/java/AnhTester/KhoiDongWeb/LoginCMS.java
@@ -0,0 +1,40 @@
+package AnhTester.KhoiDongWeb;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class LoginCMS {
+  public static void main(String[] args) throws InterruptedException {
+
+    // khoi tao browser
+    WebDriver driver;
+    WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    // Login
+    driver.get("https://crm.anhtester.com/admin/authentication");
+
+    // Bắt web element
+    
+    WebElement inputEmail = driver.findElement(By.xpath("//input[@id='email']"));
+    inputEmail.sendKeys("admin@example.com");
+
+    WebElement inputPassword = driver.findElement(By.xpath("//input[@id='password']"));
+    inputPassword.sendKeys("123456");
+
+    WebElement btnLogin = driver.findElement(By.xpath("//button[@type='submit']"));
+    btnLogin.click();
+
+    //Click button
+    Thread.sleep(3000);
+    // close browser
+    driver.quit();
+  }
+}
--- SeleniumMaven/src/test/java/AnhTester/TestSelenium.java
+++ SeleniumMaven/src/test/java/AnhTester/TestSelenium.java
@@ -1,5 +1,28 @@
 package AnhTester;
 
-public class TestSelenium {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
+public class TestSelenium {
+  public static void main(String[] args) throws InterruptedException {
+
+    // khoi tao browser
+    WebDriver driver;
+    WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    // open website
+    driver.get("https://cms.anhtester.com/");
+
+    //Click button
+    Thread.sleep(3000);
+    // close browser
+    driver.quit();
+
+  }
 }
